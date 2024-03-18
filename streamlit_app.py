d028acd6156052f124e7d61a495cd94415990a58
louisvalx <162983030+louisvalx@users.noreply.github.com>
2024-03-18 12:53:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,77 +1,8 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
-Hanzi	Pinyin	Définition
-差不多	chàbùduō	A peu près
-刻	kè	Un quart d’heure
-物理	wùlǐ	Sciences Physiques
-化学	huàxué	Chimie
-生物	shēngwù	SVT, Biologie
-动物	dòngwù	Animal
-上一次	shàng yīcì	La dernière fois
-下一次	xià yīcì	La prochaine fois
-科目	kēmù	Matière scolaire
-人口	rénkǒu	Population
-分数	fēnshù	Note
-度	dù	Degré Celsius
-季	jì	Saison
-学期	xuéqí	Trimestre
-城市	chéngshì	Ville
-城	chéng	Mur
-热闹	rènào	Animé (adj)
-方便	fāngbiàn	Pratique
-丑	chǒu	Moche
-对话	duìhuà	Dialogue
-地铁	dìtiě	Metro
-电车	diànchē	Tramway
-火车	huǒchē	Train
-自行车	zìxíngchē	Vélo
-摩托车	mótuō chē	Moto
-走路	zǒulù	Marcher
-路	lù	Chemin
-公交车	gōngjiāo chē	Bus
-交通	jiāotōng	Transport
-大巴	dàbā	Car
-出租车	chūzū chē	Taxi
-开车	kāichē	Conduire
-只有	zhǐyǒu	Seulement
-商店	shāngdiàn	Magasin
-累	lèi	Fatigué
-电影院	diànyǐngyuàn	Cinéma
-介绍	jièshào	Présenter
-地区	dìqū	Quartier
-日期	rìqí	Date
-楼房	lóufáng	Immeuble
-房子	fángzi	Maison
-房间	fángjiān	Piece, salle
-客厅	kètīng	Salon
-卧室	wòshì	Chambre
-厨房	chúfáng	Cuisine
-洗澡间	xǐzǎo jiān	Salle de bain
-花园	huāyuán	Jardin
-电梯	diàntī	Ascenseur
-厕所	cèsuǒ	Toilette
-毛笔	máobǐ	Pinceau
-茶几	chájī	Table basse
-家具	jiājù	Meuble
-搬家	bānjiā	Déménager
-桌子	zhuōzi	Table
-椅子	yǐzi	Chaise
-床	chuáng	Lit
-柜子	guìzi	Placard
-房租	fángzū	Loyer
-灯	dēng	Lumière
-台灯	tái dēng	Lampe de bureau
-书架	shūjià	Bibliothèque
-沙发	shāfā	Sofa
-书房	shūfáng	Bureau
-白色	báisè	Blanc
-黑色	hēisè	Noir
-红色	hóngsè	Rouge
-蓝色	lán sè	Bleu
-绿色	lǜsè	Vert
-黄色	huángsè	Jaune
-颜色	yánsè	Couleur
+voc=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRHSxLMq45CeyPtFGnXQaIevxQHFmbiefc0xiMiHCWbfaJD6yi8qbMEdVQksCqwPbfBiqVaWKrKBd4H/pub?output=csv")
+st.dataframe(voc)
 
 
 
